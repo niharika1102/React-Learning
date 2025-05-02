50fd1011971070f413e32412a99bbbd27986073a
niharika1102 <niharikag1102@gmail.com>
2025-05-02 22:50:09 +0530

    States task done

---
+++ react-repo/src/pages/States.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Counter from '../components/states/Counter'
+
+const States = () => {
+  return (
+    <div><Counter/></div>
+  )
+}
+
+export default States
--- react-repo/src/utils/Routes.jsx
+++ react-repo/src/utils/Routes.jsx
@@ -1,11 +1,13 @@
 import Home from "../components/home/Home";
 import Props from "../pages/Props";
 import Context from "../pages/Context"
+import States from "../pages/States";
 
 const routes = [
   { path: "/", title: "Home", component: Home },
   { path: "/props", title: "Props", component: Props },
   { path: "/react-context", title: "React Context", component: Context },
+  { path: "/states", title: "States", component: States },
 ];
 
 export default routes;
